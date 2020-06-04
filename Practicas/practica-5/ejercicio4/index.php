f2dbf8867e1ce29a3f4585989393c75f98610904
Gabriel <gabrielgolzman@gmail.com>
2020-06-04 14:35:55 -0300

    Editado README, corregido texto ejercicio 4

--- Practicas/practica-5/ejercicio4/index.php
+++ Practicas/practica-5/ejercicio4/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,16 +9,18 @@
 <?php
 session_start();
 ?>
+
 <body>
     <?php
-        if (!isset($_SESSION["contador"])){
-            $_SESSION["contador"] = 1;
-        }else{
-            $_SESSION["contador"]++;
-        } 
-?> 
+    if (!isset($_SESSION["contador"])) {
+        $_SESSION["contador"] = 1;
+    } else {
+        $_SESSION["contador"]++;
+    }
+    ?>
     <?php
-        echo "Has visitado " . ($_SESSION["contador"]) . " páginas";
-    ?> 
+    echo "Has visitado " . ($_SESSION["contador"]) . " veces la página";
+    ?>
 </body>
+
 </html>
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # entornos2020
+
 Carpeta de prácticas para la materia Entornos Gráficos 2020
 
 Integrantes del grupo:
 Domingo Ana Clara,
 Golzman Gabriel,
+Stringa Agustina,
 Zorzolo Tomás
