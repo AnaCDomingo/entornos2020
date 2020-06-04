8de6e66b5320c96910dc6e17428ef0964181b3a4
Gabriel <gabrielgolzman@gmail.com>
2020-06-04 11:43:03 -0300

    Agregado estilo ejercicio 1

--- Practicas/practica-5/ejercicio1/index.php
+++ Practicas/practica-5/ejercicio1/index.php
@@ -1,20 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="./index.css" media="screen" rel="stylesheet" type="text/css" />
+    <title>Ejercicio 1</title>
 </head>
+
 <body>
-    <p>Click para enviar mail</p>
-    <button onclick="document.write('<?php send_mail() ?>')">Enviar</button>
-<?php
-function send_mail() {
-$destinatario = "gabrielgolzman@gmail.com ";
-$asunto = "Prueba";
-$cuerpo = "Esto es una prueba de envío de correo para el ejercicio 1 de la práctica 5 a través del servidor";
-mail($destinatario,$asunto,$cuerpo);
-}
-?> 
+    <div>
+        <p>Click para enviar mail</p>
+        <button class="submit" onclick="document.write('<?php send_mail() ?>')">Enviar</button>
+        <?php
+        function send_mail()
+        {
+            $destinatario = "gabrielgolzman@gmail.com ";
+            $asunto = "Prueba";
+            $cuerpo = "Esto es una prueba de envío de correo para el ejercicio 1 de la práctica 5 a través del servidor";
+            mail($destinatario, $asunto, $cuerpo);
+        }
+        ?>
+    </div>
 </body>
+
 </html>
