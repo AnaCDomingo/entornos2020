14f546d0de425e0701a86cd5eabfc231b6b0e3fb
TomasZorzo123 <zorzolot@gmail.com>
2020-06-04 14:23:37 -0300

    Ejercicio 4, práctica 5

---
+++ Practicas/practica-5/ejercicio4/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4 PHP</title>
+</head>
+<?php
+session_start();
+?>
+<body>
+    <?php
+        if (!isset($_SESSION["contador"])){
+            $_SESSION["contador"] = 1;
+        }else{
+            $_SESSION["contador"]++;
+        } 
+?> 
+    <?php
+        echo "Has visitado " . ($_SESSION["contador"]) . " páginas";
+    ?> 
+</body>
+</html>
