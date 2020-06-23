928e02cd5f948dcf942ab804157f4758b34026a7
Gabriel <gabrielgolzman@gmail.com>
2020-06-23 14:40:53 -0300

    Realizado ejercicio 6

---
+++ Practicas/practica-7/ejercicio 6/sesiones.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Sesiones</title>
+    <link rel="stylesheet" type="text/css" href="sesiones.css" media="screen" />
+</head>
+
+<body>
+    <form action="pagina2.php" method="post">
+        Ingrese el mail del alumno a consultar:
+        <input class="inputEmail" type="text" name="mail">
+        <br>
+        <input class="submit" type="submit" value="buscar">
+    </form>
+</body>
+
+</html>
