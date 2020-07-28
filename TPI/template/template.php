51c7cb0634f0390906e054586debecaa2bd545ac
Gabriel <gabrielgolzman@gmail.com>
2020-07-28 16:48:35 -0300

    Agregado template para probar estilos y bootstrap

---
+++ TPI/template/template.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="./template.css">
+    <title>ModUTN</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="col-sm-4" style="display: flex; ;align-items:center">
+            <img src="https://lh3.googleusercontent.com/proxy/CwBMP3aSHPGBn91nFc_1aNlLDtUgECHDEcHGc-l83N4qvRjBiOk8_XLbRjxOBM7XNhDO1Rr0_fFcQXBvI1z5L7_z3pjzNH9ryF05m4vZOIIYSmqa_4xrhIOs2z4a8VtQrQQlDItgrNRcadoPkw" width="50" height="50" style="margin-right:10px" alt="logo_UTN" loading="lazy">
+            <a class="navbar-brand" href="#">Módulos UTN</a>
+        </div>
+        <div class="col-sm-4" style="display: flex; justify-content:space-between">
+            <a class="navbar-brand" href="#" id="currentTab">Solicitudes</a>
+            <a class="navbar-brand" href="#" 1>Vacantes</a>
+
+        </div>
+        <div class="col-sm-4" style="display: flex; justify-content:flex-end;align-items:center">
+            <a class="navbar-brand" href="#">Nombre</a>
+            <img src="https://cdn2.iconfinder.com/data/icons/people-80/96/Picture1-512.png" width="50" height="50" alt="person_icon" loading="lazy">
+        </div>
+    </nav>
+    </div>
+</body>
+
+</html>
