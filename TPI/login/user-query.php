60f6d766add2946b78d523849fd5445d54bc6d69
acdomingo@profitsistemas.com <anaclaradomingo@gmail.com>
2020-08-01 19:09:00 -0300

    Agregadas variables de sesión al login

--- TPI/login/user-query.php
+++ TPI/login/user-query.php
@@ -1,10 +1,18 @@
 <?php
+
+use function PHPSTORM_META\type;
+
 include("../conexion.php");
 $vEmail = $_POST['email'];
 $vPass = $_POST['pass'];
 $vSql = ("SELECT * FROM usuarios WHERE email = '$vEmail' AND pass =  '$vPass' AND id_estado <> 2 ");
 $vResultado = mysqli_query($link, $vSql);
 if (mysqli_num_rows($vResultado) > 0) {
+    session_start();
+    $fila = mysqli_fetch_array($vResultado);
+    $_SESSION['id_usuario'] = $fila['id_usuario'];
+    $_SESSION['nombre'] = $fila['nombre'];
+    setcookie('nombre', $fila['nombre'], time() + (60 * 60 * 24));
     header('Location: ../template/template.php');
 } else {
     echo '<script language="javascript">';
