21644d638ca7f82eb91253d3e380caa63feebbd5
acdomingo@profitsistemas.com <anaclaradomingo@gmail.com>
2020-08-01 19:18:24 -0300

    Agregado tipo de usuario a las variables de sesión

--- TPI/login/user-query.php
+++ TPI/login/user-query.php
@@ -12,6 +12,7 @@ if (mysqli_num_rows($vResultado) > 0) {
     $fila = mysqli_fetch_array($vResultado);
     $_SESSION['id_usuario'] = $fila['id_usuario'];
     $_SESSION['nombre'] = $fila['nombre'];
+    $_SESSION['tipo_usuario'] = $fila['id_tipo_usuario'];
     setcookie('nombre', $fila['nombre'], time() + (60 * 60 * 24));
     header('Location: ../template/template.php');
 } else {
