68383a443be29877f9d108ac6c930ffa052af315
acdomingo@profitsistemas.com <anaclaradomingo@gmail.com>
2020-08-01 21:44:40 -0300

    lalala

--- TPI/dashboard-user/dashboard.php
+++ TPI/dashboard-user/dashboard.php
@@ -1,6 +1,12 @@
 <?php
-    session_start();
+
+use function PHPSTORM_META\type;
+
+session_start();
     $nombre = $_SESSION['nombre'];
+    include_once('get-postulaciones-user.php');
+   /* include_once('pop'); */
+    $postulaciones = getList();
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +16,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="./template.css">
+    <link rel="stylesheet" href="./dashboard.css">
     <title>ModUTN</title>
 </head>
 
@@ -21,7 +27,7 @@
             <a class="navbar-brand" href="#">Módulos UTN</a>
         </div>
         <div class="col-sm-4" style="display: flex; justify-content:space-between">
-            <a class="navbar-brand" href="#" id="currentTab">Solicitudes</a>
+            <a class="navbar-brand" href="dashboard.php" id="currentTab">Postulaciones</a>
             <a class="navbar-brand" href="#" 1>Vacantes</a>
 
         </div>
@@ -34,45 +40,37 @@
         <div class="col-xs-2"></div>
         <div class="col-xs-8">
             <div class="container">
-                <h2>Título</h2>
+                <h2>Mis postulaciones</h2>
                 <hr>
-                <form>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Nombre</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Apellido</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
-                            </div>
-                            <div class="form-group">
-                                <label for="InputPassword1">Password</label>
-                                <input type="password" class="form-control" id="InputPassword1">
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Legajo/DNI</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Email</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
-                            </div>
-                            <div class="form-group">
-                                <label for="InputPassword1">Repetir Password</label>
-                                <input type="password" class="form-control" id="InputPassword1">
-                            </div>
-                        </div>
+                <div class="container-fluid">
+                    <div class="col-xs-2">
+                    </div>
+                    <div class="col-xs-10">
+                        <?php 
+                            if (is_string($postulaciones)) {
+                                echo '<h3>'.$postulaciones.'</h3>';
+                            } else {
+                                $cantidad = mysqli_num_rows($postulaciones);
+                                $cantidad = 23;
+                                if ($cantidad > 4) {
+                                    $paginado = True;
+                                    $paginas_totales = (int)($cantidad / 4);
+                                    if (fmod($cantidad,4) != 0)  {
+                                        $paginas_totales ++;
+                                    }
+                                } else {
+                                    $paginas_totales = 1;
+                                }
+                            }
+                        ?>
+                        <div class="col-xs-5"></div>
+                        <div class="col-xs-5"></div>
                     </div>
                     <div class="row" id="buttonsRow">
                         <a id="goBackButton" onclick="document.location.href='../login/login.php'" class="btn btn-danger">Volver</a>
                         <button type="submit" class="btn btn-primary"> Registrarse</button>
-                </form>
-            </div>
-
+                    </div>
+                </div>
         </div>
     </div>
     <div class="col-xs-2"></div>
