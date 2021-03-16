07e9e8e1745f4f71aad025135228ce7e1432811b
TomasZorzo123 <zorzolot@gmail.com>
2021-03-16 15:54:18 -0300

    cambios no guardados

--- TPI/configuration-user/cerrarSesion.php
+++ TPI/configuration-user/cerrarSesion.php
@@ -3,17 +3,23 @@
 # cerrar sesión de usuario (botón salir)
 # limpia las cookies y elimina la de sesión con el id de usuario y el nombre
 
-if (isset($_SERVER['HTTP_COOKIE'])) {
-    echo('entro 1');
-    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+include_once('../dashboard-user/dashboard-user.php');
+    $nombre = $_SESSION['nombre'];
+if (isset($_SERVER['HTTP_COOKIE_VARS'])) {
+    echo($nombre);
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE_VARS']);
     foreach($cookies as $cookie) {
+        echo('entra al seteo');
         $parts = explode('=', $cookie);
         $name = trim($parts[0]);
         setcookie($name, '', time()-1000);
         setcookie($name, '', time()-1000, '/');
+        unset($_SESSION['nombre']);
         session_destroy(); 
     }
 };
-mysqli_close($link);
-header('Location: ../login/login.php');
+unset($_SESSION['nombre']);
+setcookie(nombre,'', time()-1000);
+echo($nombre);
+//header('Location: ../login/login.php');
 ?>
